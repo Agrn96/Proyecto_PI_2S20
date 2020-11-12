9ec87e8abca2eb42100f17b183defe8016793ca3
Alberto Reyes <agrn96p@gmail.com>
2020-11-12 16:29:58 -0600

    Revert "Delete index.js"
    This reverts commit ea98b775d190777eaafc318d579fa9b9562919b2.

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './login_Test';
+//import App from './react_Router_Test';
+import Routes from './react_Router_Test';
+
+
+ReactDOM.render(
+    <Routes />,
+  document.getElementById('root')
+);
