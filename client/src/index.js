ad4a7909edf29d0fe746530b927ff256926506af
Alberto Reyes <agrn96p@gmail.com>
2020-11-12 16:39:01 -0600

    Modificaciones

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import './index.css';
 import Routes from './routes/Routes'
+=======
+//import App from './login_Test';
+//import App from './react_Router_Test';
+import Routes from './react_Router_Test';
+
+>>>>>>> API
 
 ReactDOM.render(
     <Routes />,
